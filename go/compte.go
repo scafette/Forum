45ef60ea62034d8c6111ac104eec8390a49ad6b9
Gio <giomascaro@yahoo.com>
2024-04-29 12:04:30 +0200

    création func Login , struct user , variable user connecté

--- go/compte.go
+++ go/compte.go
@@ -9,6 +9,22 @@ import (
 	"github.com/google/uuid"
 )
 
+type user struct {
+	Customer_id string
+	Name        string
+	Email       string
+	Phone       string
+	Created_at  string
+	Updated_at  string
+	Deleted_at  string
+	Status      string
+	Password    string
+	Role        string
+	Avatar      string
+}
+
+var ConnectedUser user
+
 func Signup(name string, email string, phone string, password string, role string, avatar string) {
 	// crée le UUID 4 pour le customer_id
 	u2, err := uuid.NewRandom()
@@ -17,7 +33,7 @@ func Signup(name string, email string, phone string, password string, role strin
 	}
 	log.Printf("generated Version 4 UUID %v", u2)
 
-	// Open a database connection
+	// Ouvre une connexion à la base de données
 	db, err := sql.Open("sqlite3", "./db.sql")
 	if err != nil {
 		fmt.Println("Error opening database:", err)
@@ -28,10 +44,38 @@ func Signup(name string, email string, phone string, password string, role strin
 	// cherche le temps pour la création et/ou update
 	currentTime := time.Now().Format("2006-01-02 15:04:05")
 
-	// Insert a user
+	// Insertion d'un utilisateur
 	_, err = db.Exec("INSERT INTO users (customer_id, name, email, phone, created_at, updated_at, status, password, role, Avatar) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", u2.String(), name, email, phone, currentTime, currentTime, password, role, avatar)
 	if err != nil {
 		fmt.Println("Error inserting user:", err)
 		return
+	}
+}
+func Login(email string, password string) {
+	// co base de données
+	db, err := sql.Open("sqlite3", "./db.sql")
+	if err != nil {
+		fmt.Println("Error opening database:", err)
+		return
+	}
+	defer db.Close()
+
+	// recherche de l'user avk Select ( stock tout dans le rows)
+	rows, err := db.Query("SELECT * FROM users WHERE email = ? AND password = ?", email, password)
+	if err != nil {
+		fmt.Println("Error selecting user:", err)
+		return
+	}
+	defer rows.Close()
+	// boucle pour parcourir les rows , et stocke les valeurs dans infoUser une fois l'user connecté ( ConnectedUser)
+	for rows.Next() {
+		var infoUser user
+		err = rows.Scan(&infoUser.Customer_id, &infoUser.Name, &infoUser.Email, &infoUser.Phone, &infoUser.Created_at, &infoUser.Updated_at, &infoUser.Deleted_at, &infoUser.Status, &infoUser.Password, &infoUser.Role, &infoUser.Avatar)
+		if err != nil {
+			fmt.Println("Error scanning user:", err)
+			return
+		}
+		fmt.Printf("customer_id: %s, name: %s, email: %s, phone: %s, created_at: %s, updated_at: %s, deleted_at: %s, status: %s, password: %s, role: %s, Avatar: %s\n", infoUser.Customer_id, infoUser.Name, infoUser.Email, infoUser.Phone, infoUser.Created_at, infoUser.Updated_at, infoUser.Deleted_at, infoUser.Status, infoUser.Password, infoUser.Role, infoUser.Avatar)
+		ConnectedUser = infoUser
 	}
 }
